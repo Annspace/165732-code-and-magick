81c1daf8a616afe63951fd7f65099a7e60d9d5a8
Ann <anya.kurenkova1@yandex.ru>
2018-04-03 02:08:24 +0300

    Fix

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
@@ -32,8 +32,8 @@ var getMaxElement = function (arr) {
 
 
 window.renderStatistics = function (ctx, names, times) {
- renderCloud(CLOUD_X + 10, CLOUD_Y + 10,'rgba(0,0,0,0.7)',ctx);
- renderCloud(CLOUD_X, CLOUD_Y,'#fff',ctx);
+ renderCloud(CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0,0,0,0.7)', ctx);
+ renderCloud(CLOUD_X, CLOUD_Y,'#fff', ctx);
  ctx.fillStyle = '#000';
  ctx.font = '16px PT Mono';
  ctx.fillText('Ура вы победили!', TEXT_PADDING_X, TEXT_VICTORY_Y);
@@ -44,14 +44,14 @@ window.renderStatistics = function (ctx, names, times) {
  for (var i = 0; i < names.length; i++ ) {
 
    height[i] = (Math.round(times[i])*GISTOGRAM_HEIGHT / maxTime);
+
     if (names[i] === 'Вы') {
      ctx.globalAlpha = 1;
-     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-   }
+     ctx.fillStyle = 'rgba(255, 0, 0, 1)'; }
+
     else {
      ctx.globalAlpha = Math.random();
-     ctx.fillStyle = 'blue';
-   }
+     ctx.fillStyle = 'blue'; }
 
    ctx.fillText(Math.round(times[i]), TEXT_PADDING_X + i*GAP, TIMES_TOP_PADDING-height[i]);
 
@@ -59,6 +59,6 @@ window.renderStatistics = function (ctx, names, times) {
 
    ctx.fillText(names[i], TEXT_PADDING_X + i*GAP, LABELS_TOP_PADDING);
 
- }
+  }
 
 };
